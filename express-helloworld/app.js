da1e45b01d1bcad6235bb3c502aa7400102d8d05
prashant <pfborkar@gmail.com>
2020-05-10 02:47:04 +0530

    add /prashant endpoint in app

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/prashant', function (req, res) {
+  res.send('Hello Prashant!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
